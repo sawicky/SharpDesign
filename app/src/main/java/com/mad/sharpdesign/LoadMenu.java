ef0ac9a967ce17f014f8036e93cc1154020993ee
sawicky <sawickdavid@protonmail.com>
2018-09-16 18:30:33 +1000

    initial commit

---
+++ app/src/main/java/com/mad/sharpdesign/LoadMenu.java
@@ -0,0 +1,13 @@
+package com.mad.sharpdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_menu);
+    }
+}
